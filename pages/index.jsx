9ece6a78bf6e5d57a5646e579996711e0a64912c
Alex <aleksander.karp2000@gmail.com>
2020-04-16 12:18:39 +0200

    Add favicons

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,14 +11,34 @@ import { socialMediaData } from '../data/socialMedia';
 //   () => import('../components/Messenger/Messenger'),
 //   { ssr: false }
 // );
-//
-// Await for credentials
+//Await for credentials
 
 const Home = () => (
   <div>
     <Head>
       <title>PetsCity</title>
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4052f2" />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="theme-color" content="#d7e0fd" />
     </Head>
 
     <Navbar />
