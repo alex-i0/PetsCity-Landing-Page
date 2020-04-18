cd111811150846446ff73a8eff4604eb21372990
Alex <aleksander.karp2000@gmail.com>
2020-04-18 12:58:14 +0200

    Add PWA configuration

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,10 +19,19 @@ const Home = () => (
   <div>
     <Head>
       <title>PetsCity</title>
-
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
       <meta
         name="description"
         content="Join to Pets City today! Get access to hundreds of certified breeders."
+      />
+      <meta
+        name="keywords"
+        content="Pets, City, Pets.city, animals transport, animals to buy"
       />
 
       <link rel="icon" href="/favicon.ico" />
@@ -43,7 +52,7 @@ const Home = () => (
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="/manifest.json" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4052f2" />
       <meta name="msapplication-TileColor" content="#2b5797" />
       <meta name="theme-color" content="#d7e0fd" />
