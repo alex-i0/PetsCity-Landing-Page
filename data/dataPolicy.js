aef394632eabb238da1b1eb0d9b7eaae78180c1d
Alex <aleksander.karp2000@gmail.com>
2020-07-07 14:13:14 +0200

    Replace text holders in data policy file

--- data/dataPolicy.js
+++ data/dataPolicy.js
@@ -1,32 +1,47 @@
 export const dataPolicy = [
   {
-    title: 'How do we collect your data?',
+    title: 'Privacy Policy for Pets City',
     text:
-      'The Swiss psychologist and psychiatrist Carl Jung was one of the major forces responsible for bringing psychological (having to do with the mind and its processes) thought and its theories into the twentieth century.',
+      'At Pets City, accessible from pets.city, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Pets City and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.\nThis Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Pets City. This policy is not applicable to any information collected offline or via channels other than this website.',
   },
   {
-    title: 'What is a cookie?',
+    title: 'Consent',
     text:
-      'The Swiss psychologist and psychiatrist Carl Jung was one of the major forces responsible for bringing psychological (having to do with the mind and its processes) thought and its theories into the twentieth century.',
+      'By using our website, you hereby consent to our Privacy Policy and agree to its terms.',
   },
   {
-    title: 'Privacy policies of other websites',
+    title: 'Information we collect',
     text:
-      'The Swiss psychologist and psychiatrist  Carl Jung was one of the major forces responsible for bringing psychological (having to do with the mind and its processes) thought and its theories into the twentieth century.',
+      'The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.',
   },
   {
-    title: 'Complaint Submission',
-    text:
-      'The Swiss psychologist and psychiatrist Carl Jung was one of the major forces responsible for bringing psychological (having to do with the mind and its processes) thought and its theories into the twentieth century.',
+    title: 'How we use your information',
+    text: `We use the information we collect in various ways, including to:
+      Provide, operate, and maintain our website, understand and analyze how you use our website, develop new products, services, features, and functionality, communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the webste, and for marketing and promotional purposes, send you emails, find and prevent fraud.`,
   },
   {
-    title: 'Our Purpose and Legal Basis of Data We Collect',
+    title: 'Log Files',
     text:
-      'The Swiss psychologist and psychiatrist Carl Jung was one of the major forces responsible for bringing psychological (having to do with the mind and its processes) thought and its theories into the twentieth century.',
+      "Pets City follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information. Our Privacy Policy was created with the help of the Privacy Policy Generator and the Online Privacy Policy Generator.",
   },
   {
-    title: 'How do we store your data?',
+    title: 'Cookies and Web Beacons',
     text:
-      'The Swiss psychologist and psychiatrist Carl Jung was one of the major forces responsible for bringing psychological (having to do with the mind and its processes) thought and its theories into the twentieth century.',
+      "Like any other website, Pets City uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.",
+  },
+  {
+    title: 'Advertising Partners Privacy Policies',
+    text:
+      "You may consult this list to find the Privacy Policy for each of the advertising partners of Pets City. Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Pets City, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit. Note that Pets City has no access to or control over these cookies that are used by third-party advertisers.",
+  },
+  {
+    title: 'Third Party Privacy Policies',
+    text:
+      "Pets City's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.",
+  },
+  {
+    title: 'GDPR Data Protection Rights',
+    text:
+      'We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following: The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service. The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete. The right to erasure – You have the right to request that we erase your personal data, under certain conditions. The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions. The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions. The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions. If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.',
   },
 ];
