ed4dd21290659c1724285baa82ac99c208a9207f
Alek Karp <aleksander.karp2000@gmail.com>
2020-11-18 10:57:39 +0100

    Remove PWA support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 const withSass = require('@zeit/next-sass');
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
 
-module.exports = withSass(withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching
-  },
-}));
+module.exports = withSass();
