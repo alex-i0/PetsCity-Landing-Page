94adc67e2191c868db078cbf7bfa97e99c6961b3
Alek Karp <aleksander.karp2000@gmail.com>
2020-12-07 15:08:36 +0100

    Add animals placeholder data

---
+++ data/animals.js
@@ -0,0 +1,27 @@
+export const AnimalsArray = [
+    {
+        animalType: 'Chihuahua',
+        breeder: 'Chili Burritos FCI',
+        image: '/assets/ab/ab_chi.jpg'
+    },
+    {
+        animalType: 'Amstaff',
+        breeder: 'Gang Staff',
+        image: '/assets/ab/ab_amstaff.jpg'
+    },
+    {
+        animalType: 'Ragdol',
+        breeder: 'Dom Elzy - Ragdolls',
+        image: '/assets/ab/ab_ragdoll.jpg'
+    },
+    {
+        animalType: 'Owczarek niemiecki',
+        breeder: 'ZKWP FCI Wrocław',
+        image: '/assets/ab/ab_owczarek.jpg'
+    },
+    {
+        animalType: 'Boston Terrier',
+        breeder: 'Ars Rottis FCI',
+        image: '/assets/ab/ab_boston.jpg'
+    }
+];
