988181f718762b0c427c3e7756d07606c49233e8
Alek Karp <aleksander.karp2000@gmail.com>
2021-01-03 14:58:18 +0100

    Add Counter Component

--- components/CustomComponents/Counter/Counter.jsx
+++ components/CustomComponents/Counter/Counter.jsx
@@ -1,7 +1,34 @@
+import Typography from '../../Typography/Typography';
+
+const appStatistics = [
+    {
+        title: 'Breeders',
+        number: 200,
+        monthlyChange: 26
+    },
+    {
+        title: 'Pets Types',
+        number: 56,
+        monthlyChange: 8
+    },
+    {
+        title: 'Waitlist Users',
+        number: 489,
+        monthlyChange: 54
+    }
+];
+
 const Counter = () => (
     <div className="counter-container">
         <div className="counter-card">
-            <div></div>
+            {appStatistics.map((statistic) => (
+                <div className="single-counter">
+                    <Typography type="text" size="large">
+                        {statistic.number}
+                    </Typography>
+                    <span>This month</span>
+                </div>
+            ))}
         </div>
     </div>
 );
