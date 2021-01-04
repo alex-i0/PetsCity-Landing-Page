ea9f8253745811fb79e117050fd8cd5a6dd17288
Alek Karp <aleksander.karp2000@gmail.com>
2021-01-04 17:28:13 +0100

    Add Api Keys

--- components/Messenger/Messenger.jsx
+++ components/Messenger/Messenger.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-const Messenger = () => <MessengerCustomerChat pageId="2212800995661774" appId="570611146902725" />;
+const Messenger = () => <MessengerCustomerChat pageId={process.env.FB_PAGE_ID} appId={process.env.FB_APP_ID} />;
 
 export default Messenger;
