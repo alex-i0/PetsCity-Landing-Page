ffae9854d314d33042015a2ff9a6bd56b23d5a33
Alek Karp <aleksander.karp2000@gmail.com>
2021-01-04 17:53:05 +0100

    Add API Keys to Variables

--- components/Messenger/Messenger.jsx
+++ components/Messenger/Messenger.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-const Messenger = () => <MessengerCustomerChat pageId={process.env.FB_PAGE_ID} appId={process.env.FB_APP_ID} />;
+const Messenger = () => {
+    const PAGE_ID = process.env.FB_PAGE_ID;
+    const APP_ID = process.env.FB_APP_ID;
+    return <MessengerCustomerChat pageId={PAGE_ID} appId={APP_ID} />;
+};
 
 export default Messenger;
