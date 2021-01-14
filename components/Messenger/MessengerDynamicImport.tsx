ec326e48d3667390e839909d0d1adeb08f45b736
Alek Karp <aleksander.karp2000@gmail.com>
2021-01-14 18:08:49 +0100

    Update messenger-chat

--- components/Messenger/MessengerDynamicImport.tsx
+++ components/Messenger/MessengerDynamicImport.tsx
@@ -2,8 +2,6 @@ import dynamic from 'next/dynamic';
 
 const DynamicMessenger = dynamic(() => import('./Messenger'), { ssr: false });
 
-const MessengerDynamicImport: React.FC = () => {
-    return <DynamicMessenger />;
-};
+const MessengerDynamicImport: React.FC = () => <DynamicMessenger />;
 
 export default MessengerDynamicImport;
